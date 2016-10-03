546b71a9d3d7976fc205af50ad30d44d9c994db0
leeson9394 <leeson9394@gmail.com>
2016-10-03 17:38:08 -0400

    Add files via upload
    Update integer range.

--- Reverse_number_v2.py
+++ Reverse_number_v2.py
@@ -1,7 +1,11 @@
-input_num=input("Enter an integer: ")
-bin_input=[int(i) for i in str(bin(input_num))[2:]]
+import sys	
 
-bin_input.reverse()
-bin_output=int(''.join(map(str,bin_input)),2)
+input_num=input("Enter an integer N that 1 <= N <= 1000000000: ")
+if 1 <= input_num <= 1000000000
+	bin_input=[int(i) for i in str(bin(input_num))[2:]]
+	bin_input.reverse()
+	bin_output=int(''.join(map(str,bin_input)),2)
 
-print "Output integer: ",bin_output
+	print "Output integer: ",bin_output
+else:
+	print "Input number not in range"
